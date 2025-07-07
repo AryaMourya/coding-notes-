959a1254b13f90fec0775f882308d7f7e3221fe5
Arya Mourya <aryamourya0@gmail.com>
2025-07-07 21:52:32 +0530

    update class-python-notes.py

--- class-python-notes.py
+++ class-python-notes.py
@@ -139,14 +139,26 @@ print("Run this loop")
 # Guessing Game:-
 secret_word = "giraffe"
 guess =""
-hint=input("DO you want hints?")
-if hint=="Yes":
-    print ("wild animal" \
-    "eat grass" \
-    "has long neck")
-elif hint=="No":
-    print("OK!")
-while guess !=secret_word:
-    guess = input("Enter guess: ")
-
-print("You win!")
+guess_count=0
+guess_limit=3
+out_of_guesses=False
+hint_taken=False
+while guess !=secret_word and not out_of_guesses:
+    if guess_limit > guess_count:
+      guess = input("Enter guess: ")
+      guess_count+=1
+      if not hint_taken and guess !=secret_word:
+         hint=input("DO you want hints? (Yes/No):")
+         if hint.lower()=="Yes":
+           print ("wild animal," \
+                   "eat grass," \
+                   "has long neck")
+         elif hint.lower()=="No":
+              print("OK!")
+    else:
+        out_of_guesses=True
+       
+if out_of_guesses==True:
+ print("You lose!")
+else:
+  print("You Win!")
