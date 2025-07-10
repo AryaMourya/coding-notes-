f7327fbf84d8d442ca53e0b1c7a63d3a99bdce25
Arya Mourya <aryamourya0@gmail.com>
2025-07-10 22:27:19 +0530

    update python oop notes

--- python-oop-notes.py
+++ python-oop-notes.py
@@ -1,1 +1,13 @@
-### INTRODUCTION ###
+class Person:
+    name = "Arya"
+    occupation = "Student"
+    gender = "female"
+    def info(self):
+        print(f"{self.name} is a {self.occupation} .")
+
+a = Person()
+a.name = "Anu"
+a.occupation = "Lawyer"
+a.gender = "female"
+
+print(a.name,a.gender,a.occupation)
