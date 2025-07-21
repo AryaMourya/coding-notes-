b71ae90193cca04b662c70349397f4e372a1f012
Arya Mourya <aryamourya0@gmail.com>
2025-07-21 11:52:53 +0530

    update opp topic 1 notes

--- oop python/class-and-objects.py
+++ oop python/class-and-objects.py
@@ -15,23 +15,33 @@ class Dog :
         print("Woof Woof")
 
 class Owner :
-    def __init__(self,name,address,contact_number):
+    def __init__(self,name,address,contact_number,emailID):
         self.name = name
         self.address = address
         self.contact_number = contact_number
+        self.emailID = emailID
+    
+    def introduction(self,Dog):
+        print(f"Hi!,I'm {self.name},I lives {self.address} .I would like to introduce you to my pet .")
+        print(f"This is {Dog.name} ,my pet dog and he is a {Dog.breed}")
 
-Owner1 = Owner("Rikki","xyz road","0000123499")
+
+Owner1 = Owner("Rikki","xyz road","0000123499","rikki01@gmail.com")
 dog1 = Dog("Bruce","Pitbull",Owner1)
 dog1.bark()
+Owner1.introduction(dog1)
 print(dog1.name)
 print(dog1.breed)
 print(dog1.Owner.name)
+print(dog1.Owner.contact_number,dog1.Owner.emailID)
 
-Owner2 = Owner("Simmi","abcd road","999993447")
-dog2 = Dog("Freya","ScoOwner1ttish Terrior")
+Owner2 = Owner("Simmi","abcd road","999993447","simmi23@gmail.com")
+dog2 = Dog("Freya","ScoOwner1ttish Terrior",Owner2)
 dog2.bark()
+Owner2.introduction(dog2)
 print(dog2.name)
 print(dog2.breed)
+print(dog2.Owner.contact_number,dog2.Owner.emailID)
 
 class Person:
     def __init__(self,name,age):
