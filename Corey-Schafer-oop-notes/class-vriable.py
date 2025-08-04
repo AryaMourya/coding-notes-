14789375ae112e1352574d81355fef661549c1dd
Arya Mourya <aryamourya0@gmail.com>
2025-08-04 16:44:33 +0530

    Update and add note in class variable notes

--- Corey-Schafer-oop-notes/class-vriable.py
+++ Corey-Schafer-oop-notes/class-vriable.py
@@ -9,10 +9,16 @@ class Employee:
         return '{} {}'.format(self.first.capitalize(),self.last.capitalize())
     
     def raise_applied(self):
-        self.pay = int(self.pay * 1.04)
+        self.pay ="Rs" + str((int(self.pay * 1.04)))
 
 Employee_1 = Employee("arya","mourya",56000)
 
 print(Employee_1.pay)
 Employee_1.raise_applied()
 print(Employee_1.pay)
+
+''' Class-Variable = A variable that is shared by all instances of a class.
+   It is defined within the class but outside any instance methods.'''
+# Class variables are accessed using the class name or through an instance of the class.
+#class_var = "This is a class variable"
+#like instance variable is unique to each instance, class variable should be same for each instance.
