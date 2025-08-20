b3a7ffacb2e0c0722daff5fdcd3b203c7e21de2e
Arya Mourya <aryamourya0@gmail.com>
2025-08-20 18:36:49 +0530

    Update and add file in this folder

--- Corey-Schafer-oop-notes/class-vriable.py
+++ Corey-Schafer-oop-notes/class-vriable.py
@@ -1,3 +1,7 @@
+raise_amount = 1.04
+
+
+
 class Employee:
     def __init__(self,first,last,pay):
         self.first = first
@@ -12,10 +16,11 @@ class Employee:
         self.pay ="Rs" + str((int(self.pay * 1.04)))
 
 Employee_1 = Employee("arya","mourya",56000)
+Employee_2 = Employee("Dona rona",8900)
 
 print(Employee_1.pay)
-Employee_1.raise_applied()
-print(Employee_1.pay)
+Employee.raise_applied()
+print(Employee.pay)
 
 ''' Class-Variable = A variable that is shared by all instances of a class.
    It is defined within the class but outside any instance methods.'''
