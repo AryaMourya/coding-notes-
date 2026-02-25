908fbdd0b7446592b2a22213c20e878c790de4f8
AryaMourya <aryamourya0@gmail.com>
2026-02-25 23:03:13 +0530

    add multiple functions for greetings and messages in Python

--- python notes/function-in-python.py
+++ python notes/function-in-python.py
@@ -1,2 +1,27 @@
 #function in python 
 
+def sumfunc() :
+    print("Arya Mourya")
+
+sumfunc()
+sumfunc()
+sumfunc()
+
+
+#practice question 
+#1
+def welcome_message():
+    print("Welcome to Python Programming!")
+
+welcome_message()
+welcome_message()
+welcome_message()
+#2
+def inspire():
+    print("que serra que serra ~arya mourya")
+
+inspire()
+#3
+def good_morning():
+    print("Good Morning,Saumya!")
+
