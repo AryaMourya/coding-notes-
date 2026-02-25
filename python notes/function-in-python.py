92d92c7450f25a34715c9d1805aebd0dd0d63d89
AryaMourya <aryamourya0@gmail.com>
2026-02-25 23:16:40 +0530

    add greet function and improve formatting in function-in-python.py

--- python notes/function-in-python.py
+++ python notes/function-in-python.py
@@ -16,20 +16,28 @@ def welcome_message():
 welcome_message()
 welcome_message()
 welcome_message()
+
 #2
 def inspire():
     print("que serra que serra ~arya mourya")
 
 inspire()
+
 #3
 def good_morning():
     print("Good Morning,Saumya!")
 
 good_morning()
 good_morning()
+
 #4
 def learn():
     print("function")
     print("dictionary")
     print("tuples")
 learn()
+
+##########Function Parameter & Arguments#######
+def greet(name):
+    print("Hello",name)
+greet("Arya Mourya")
