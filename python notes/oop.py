62417924281b3623843e5cf77b474349ce220ced
AryaMourya <aryamourya0@gmail.com>
2026-03-12 03:28:50 +0530

    comit and save all the changes

--- python notes/oop.py
+++ python notes/oop.py
@@ -82,10 +82,12 @@ class Student:
         self.listOfMarks=listOfMarks
     
     def average(self):
+        sum=0
         for eachvalue in self.listOfMarks:
-            sum=sum+eachvalue
+            sum= sum+eachvalue
 
         average= sum/3
         print("Average is",average)
 
 student1=Student("Aditya",[90,98,99])
+student1.average()
