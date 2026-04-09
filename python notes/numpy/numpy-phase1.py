2e48c7bda7d23c2f75a3fa31e5ee5b8d3e0723bf
AryaMourya <aryamourya0@gmail.com>
2026-04-09 19:33:48 +0530

    commit and save all changes

--- python notes/numpy/numpy-phase1.py
+++ python notes/numpy/numpy-phase1.py
@@ -1,3 +1,10 @@
 ## Numpy array and basics
 
 import numpy as np
+
+#creating array from list 
+arr_1d = np.array([1,2,3,4,5])
+print("1D array:",arr_1d)
+
+arr_2d = np.array([1,2,3],[4,5,6])
+print("2D array:",arr_2d) 
