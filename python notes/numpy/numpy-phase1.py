f7635210a0dbbe5c20b924971632ffafcf783d21
AryaMourya <aryamourya0@gmail.com>
2026-04-09 19:58:44 +0530

    commit and save all changes

--- python notes/numpy/numpy-phase1.py
+++ python notes/numpy/numpy-phase1.py
@@ -6,5 +6,13 @@ import numpy as np
 arr_1d = np.array([1,2,3,4,5])
 print("1D array:",arr_1d)
 
-arr_2d = np.array([1,2,3],[4,5,6])
-print("2D array:",arr_2d) 
+arr_2d = np.array([[1,2,3],[4,5,6]])
+print("2D array:",arr_2d)  
+
+### Lists vs numpy array
+
+py_list = [1,2,3]
+print("python list multiplication ", py_list * 2)
+
+np_array = np.array([1,2,3])
+print("python list multiplication ", np_array * 2)
