4602efa5189a65afca4057f2573dac17687c1477
AryaMourya <aryamourya0@gmail.com>
2026-06-16 19:17:31 +0530

    commit and save all changes

--- python notes/numpy/numpylec1.py
+++ python notes/numpy/numpylec1.py
@@ -1,4 +1,14 @@
 #Creatig Numpy Arrays#
 """np.array"""
 import numpy as np
-np.array([1,2,3])
+a = np.array([1,2,3])
+print(a)
+print(type(a))
+
+#2D and 3D
+b = np.array([[1,2,3],[4,5,6]])
+print(b)
+c = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(c)
+
+#dtype(datatype as per as our requirement)
