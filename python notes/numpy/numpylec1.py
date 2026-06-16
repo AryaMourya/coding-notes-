a306a71ee1d60d824f430090441850c72a6af269
AryaMourya <aryamourya0@gmail.com>
2026-06-16 20:42:23 +0530

    commit and save all changes

--- python notes/numpy/numpylec1.py
+++ python notes/numpy/numpylec1.py
@@ -12,3 +12,13 @@ c = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
 print(c)
 
 #dtype(datatype as per as our requirement)
+
+x=np.array([1,2,3],dtype=float)
+y=np.array([1,2,3],dtype=bool)
+k=np.array([1,2,3],dtype=complex)
+print(x)
+print(y)
+print(k)
+
+
+## np.range
