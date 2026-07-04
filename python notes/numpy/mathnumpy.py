aef22433accf7539bdc7a71fc3eadf6e79056ca4
AryaMourya <aryamourya0@gmail.com>
2026-07-04 14:35:17 +0530

    commit all changes

--- python notes/numpy/mathnumpy.py
+++ python notes/numpy/mathnumpy.py
@@ -7,8 +7,19 @@ print(np.sum(a))  # sum will add all the number in the array
 print(np.sin(a)) # sin of all the value of array
 
 """Sigmoid"""
+
 def sigmoid(array):
-    return 1/(1 + np.exp(array))
+        return 1/(1 + np.exp(-(array)))
 
 b = np.arange(10)
-sigmoid(b)
+print(sigmoid(b))
+
+"""Mean Squared Error"""
+actual = np.random.randint(1,50,25)
+predicted = np.random.randint(1,50,25)
+
+print(actual)
+print(predicted)
+
+def mse(arrays):
+        return np.mean((actual - predicted)**2)
