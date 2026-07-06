6184a20d2b1bcf389d65bc58c48fb918845dbb59
AryaMourya <aryamourya0@gmail.com>
2026-07-06 19:38:54 +0530

    commit and save all changes

--- python notes/numpy/ploting-graph.py
+++ python notes/numpy/ploting-graph.py
@@ -11,3 +11,24 @@ y = x
 
 plt.plot(x,y)
 plt.show()
+
+# y = x*2
+x = np.linspace(-50,50,100)
+y = x**2
+
+plt.plot(x,y)
+plt.show()
+
+# y = sin(x)
+x = np.linspace(-10,10,100)
+y= np.sin(x)
+
+plt.plot(x,y)
+plt.show()
+
+# y = xlog(x)
+x = np.linspace(-50,50,200)
+y = x*np.log(x)
+
+plt.plot(x,y)
+plt.show()
