a2109288d0b622a6d10905179a53ebe82cda4373
AryaMourya <aryamourya0@gmail.com>
2026-07-16 09:44:22 +0530

    commit and save all changes

--- python notes/pandas/panda-basics.py
+++ python notes/pandas/panda-basics.py
@@ -4,3 +4,26 @@ import numpy as np
 """ Series from lists """
 country = ['India','USA','UK','Nepal']
 print(pd.Series(country))
+
+""" Integers """
+runs = [11,23,50,69,78,100]
+print(pd.Series(runs))
+
+""" Custom Index """
+marks = [67,57,85,100]
+subjects = ['maths','english','science','hindi']
+print(pd.Series(marks))
+print(pd.Series(marks, index=subjects))
+
+""" Setting a name """
+print(pd.Series(marks,index=subjects,name="xyz's marks"))
+
+""" Series from dict """
+marks ={
+    'maths': 67,
+    'english':57,
+    'science':89,
+    'hindi':100 
+}
+marks_series = pd.Series(marks)
+print(marks_series)
