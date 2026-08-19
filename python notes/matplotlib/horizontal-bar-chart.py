f3fc157d9847ea1a8fed23a9b51a6fd3b13d6050
AryaMourya <aryamourya0@gmail.com>
2026-08-19 11:51:16 +0530

    horizontal bar chart update

--- python notes/matplotlib/horizontal-bar-chart.py
+++ python notes/matplotlib/horizontal-bar-chart.py
@@ -1,11 +1,42 @@
 import csv
 import numpy as np
+import pandas as pd
+from collections import Counter
 from matplotlib import pyplot as plt
 
 plt.style.use("fivethirtyeight")
 
-with open ("data.csv") as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+data = pd.read_csv('C:\Users\lenovo\coding-notes-\python notes\matplotlib\data.csv')
+ids = data['Responder_id']
+lang_responses = data['Responder_id']
 
-    row = next(csv_reader)
-    print(row)
+Language_counter = Counter()
+
+for response in lang_responses:
+        Language_counter.update(response['LanguagesWorkedWith'].split(';'))
+
+
+
+Languages = []
+popularity = []
+
+for item in Language_counter.most_common(15):
+    Languages.append(item[0])
+    popularity.append(item[1])
+
+print(Languages)
+print(popularity)
+
+Languages.reverse()
+popularity.reverse()
+
+plt.barh(Languages,popularity)
+
+plt.title("Most popular Languages")
+
+plt.xlabel("User")
+
+plt.tight_layout()
+
+plt.show()
+
